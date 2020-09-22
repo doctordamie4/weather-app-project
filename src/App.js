f250f229fe381e31b1c2711447591534423553ee
doctordamie4 <67023315+doctordamie4@users.noreply.github.com>
2020-09-21 21:06:15 -0700

    added github Repository URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App"> WeatherApp
+<footer>
+  This app was coded by Nimz and it's open-sourced
+    <a
+    href="https://github.com/doctordamie4/weather-app-project" target="_blank"> open-sourced on Github </a>
+    </footer>
     </div>
   );
 }

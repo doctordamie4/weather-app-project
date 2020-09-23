cf85c3bdb3061e129586df33f078be043359c4c5
doctordamie4 <67023315+doctordamie4@users.noreply.github.com>
2020-09-22 20:35:50 -0700

    added boostrap

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 <footer>
   This app was coded by Nimz and it's open-sourced
     <a
-    href="https://github.com/doctordamie4/weather-app-project" target="_blank"> open-sourced on Github </a>
+    href="https://github.com/doctordamie4/weather-app-project"   rel="noopener noreferrer"target="_blank" > open-sourced on Github </a>
     </footer>
     </div>
   );
